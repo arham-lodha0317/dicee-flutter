cdad6753e1b2bd2e6084da6d7d84db958d5b5868
arham-lodha <arhamsmita@gmail.com>
2020-04-27 18:43:45 -0500

    Finished Dice App
    Dice App is finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left = 1;
+  int right = 1;
+
+  void changeDice() {
+    setState(() {
+      left = Random().nextInt(6) + 1;
+      right = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$left.png'),
+              onPressed: () {
+                changeDice();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$right.png'),
+              onPressed: () {
+                changeDice();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
